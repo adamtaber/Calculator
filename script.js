e29d320da55cc7a61a9aa4abc16b914c101fd174
Adam Taber <altaber@gmail.com>
2022-05-27 16:47:30 -0500

    Add toggle to operators

--- script.js
+++ script.js
@@ -30,4 +30,84 @@ function operate(valueOne, operator, valueTwo) {
     }
 }
 
+/* create function to populate textbox when buttons are pressed */
+
+let numberButtons = document.querySelectorAll('div.numeric > button');
+let numberScreen = document.getElementById("numberScreen");
+let clearButton = document.getElementById("clear");
+
+numberButtons.forEach(btn => btn.addEventListener('click', function() {
+    numberScreen.innerHTML += this.innerHTML;
+}))
+
+clearButton.addEventListener('click', function() {
+    numberScreen.innerHTML = "";
+})
+
+let displayValue = numberScreen.innerHTML;
+
+let operatorButtons = document.querySelectorAll('div.nonNumeric > button');
+let operator;
+let operatorAdd = document.querySelector("div.nonNumeric > .add");
+let operatorClass = operatorAdd.classList;
+/*let operatorSubtract = document.querySelector("div.nonNumeric > .subtract");
+let operatorClassSubtract = operatorSubtract.classList;
+let operatorMultiply = document.querySelector("div.nonNumeric > .multiply");
+let operatorClassMultiply = operatorMultiply.classList;
+let operatorDivide = document.querySelector("div.nonNumeric > .divide");
+let operatorClassDivide = operatorDivide.classList;*/
+let toggle;
+
+operatorButtons.forEach(btn => btn.addEventListener('click', function () {
+    operator = this.innerHTML;
+    toggle = operatorClass.toggle("true")
+    if(toggle) {
+        this.style.backgroundColor = 'white';
+            this.style.color = 'rgb(255,132,0)'
+    }else {
+        this.style.backgroundColor = 'rgb(255,132,0)'
+        this.style.color = 'white';
+    }
+    /*if(this.innerHTML === "+") {
+        operator = "+";
+        toggle = operatorClass.toggle("True")
+        if(toggle) {
+            this.style.backgroundColor = 'white';
+            this.style.color = 'rgb(255,132,0)'
+        }else {
+            this.style.backgroundColor = 'rgb(255,132,0)'
+            this.style.color = 'white';
+        }
+    }else if(this.innerHTML === "-") {
+        operator = "-";
+        toggle = operatorClass.toggle("testTwo")
+        if(toggle) {
+            this.style.backgroundColor = 'white';
+            this.style.color = 'rgb(255,132,0)'
+        }else {
+            this.style.backgroundColor = 'rgb(255,132,0)'
+            this.style.color = 'white';
+        }
+    }else if(this.innerHTML === "*") {
+        operator = "*";
+        toggle = operatorClass.toggle("testTwo")
+        if(toggle) {
+            this.style.backgroundColor = 'white';
+            this.style.color = 'rgb(255,132,0)'
+        }else {
+            this.style.backgroundColor = 'rgb(255,132,0)'
+            this.style.color = 'white';
+        }
+    }else if(this.innerHTML === "/") {
+        operator = "/";
+        toggle = operatorClass.toggle("testTwo")
+        if(toggle) {
+            this.style.backgroundColor = 'white';
+            this.style.color = 'rgb(255,132,0)'
+        }else {
+            this.style.backgroundColor = 'rgb(255,132,0)'
+            this.style.color = 'white';
+        }
+    }*/
+}))
 
